eccdb74e5abc18d51f3d20ff8caa484e311df816
Duy Pham <duypham1398@gmail.com>
2021-01-29 06:50:35 +0700

    final

--- calculator.js
+++ calculator.js
@@ -1,28 +1,22 @@
-function Addition() {
     var numeberone = parseFloat(document.getElementById('numberone').value);
     var numbertwo = parseFloat(document.getElementById('numbertwo').value);
+
+function Addition() {
     result = numeberone + numbertwo;
     document.getElementById('result').innerHTML = "Result Addition: " + result;
 }
 
 function Subtraction() {
-    var numeberone = parseFloat(document.getElementById('numberone').value);
-    var numbertwo = parseFloat(document.getElementById('numbertwo').value);
     result = numeberone - numbertwo;
     document.getElementById('result').innerHTML = "Result Addition: " + result;
 }
 
 function Division() {
-    var numeberone = parseFloat(document.getElementById('numberone').value);
-    var numbertwo = parseFloat(document.getElementById('numbertwo').value);
     result = numeberone / numbertwo;
     document.getElementById('result').innerHTML = "Result Addition: " + result;
 }
 
 function Multiplication() {
-    var numeberone = parseFloat(document.getElementById('numberone').value);
-    var numbertwo = parseFloat(document.getElementById('numbertwo').value);
     result = numeberone * numbertwo;
     document.getElementById('result').innerHTML = "Result Addition: " + result;
 }
-
